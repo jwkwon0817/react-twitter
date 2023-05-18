c3b51282d2b206617b2a9eb92eeda9fe777beb80
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-18 22:16:13 +0900

    index JS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import firebase from './firebase';
 import reportWebVitals from './reportWebVitals';
+
+console.log(firebase);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
